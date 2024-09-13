aed8ec17031e055739faf2cf3a03a0db44dbf30a
cieko <rahulsaxena2021@hotmail.com>
2024-09-13 21:09:40 +0530

    theme and logo input

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -2,13 +2,14 @@
 
 import { useState } from "react"
 import { SignInFlow } from "../types"
+import { Button } from "@/components/ui/button"
 
 export const AuthScreen = () => {
 
   const [state, setState] = useState<SignInFlow>("signIn")
   return (
-    <div className="h-full w-full flex items-center justify-center bg-[#715caa]">
-      Auth Screen 1
+    <div className="h-full w-full flex items-center justify-center ">
+      <Button variant={'woks'}>Hey</Button>
     </div>
   )
 }
